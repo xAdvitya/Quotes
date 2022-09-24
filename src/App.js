e7ab241f0a19b35b75c722579bed7287904f4c11
xAdvitya <advityasharma21@gmail.com>
2022-09-25 03:47:46 +0530

    adding components, layouts and nav bar

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
-import { Redirect, Route } from 'react-router-dom';
+import { Route, Switch, Redirect } from 'react-router-dom';
 import AllQuotes from './Pages/AllQuotes';
 import NewQuote from './Pages/NewQuote';
 import QuoteDetail from './Pages/QuoteDetail';
+import Layout from './components/layout/Layout';
 
 function App() {
   return (
-    <div>
-      <switch>
-        {/* <Redirect path="/" to="/quotes" /> */}
-        
-        <Route path="/quotes">
+    <Layout>
+      <Switch>
+        <Route path="/" exact>
+          <Redirect to="/quotes" />
+        </Route>
+
+        <Route path="/quotes" exact>
           <AllQuotes />
         </Route>
         <Route path="/quotes/:quoteId">
@@ -18,8 +21,8 @@ function App() {
         <Route path="/new-quote">
           <NewQuote />
         </Route>
-      </switch>
-    </div>
+      </Switch>
+    </Layout>
   );
 }
 
--- src/Pages/QuoteDetail.js
+++ src/Pages/QuoteDetail.js
@@ -1,4 +1,17 @@
+import { Route, useParams } from 'react-router-dom';
+import Comments from '../components/comments/Comments';
 const QuoteDetail = () => {
-  return <h1>New Quote</h1>;
+  const param = useParams();
+
+  return (
+    <div>
+      <h1>Quote Details </h1>
+      <h1>{param.quoteId}</h1>
+
+      <Route path={`/quotes/${param.quoteId}/comments`}>
+        <Comments />
+      </Route>
+    </div>
+  );
 };
 export default QuoteDetail;
